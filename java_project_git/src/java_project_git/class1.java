f68eeefbda785647b01eb55f53e6c9d1735c1f03
Sandman04 <Sandman04@DESKTOP-FTTD12M>
2019-08-08 02:47:06 +0430

    delete

--- java_project_git/src/java_project_git/class1.java
+++ java_project_git/src/java_project_git/class1.java
@@ -14,7 +14,7 @@ public class class1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hassan");
+
 
 System.out.println("kharab bil had ");
 	}
