ebc873c989dc914fbc8fd845a4c2ca9f917979f6
Hassan <Hassan@DESKTOP-UQ2VU9F>
2019-08-07 14:53:13 +0430

    staarting the project  1st

---
+++ java_project_git/src/java_project_git/class1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package java_project_git;
+
+/**
+ * @author Hassan
+ *
+ */
+public class class1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hassan");
+	}
+
+}
